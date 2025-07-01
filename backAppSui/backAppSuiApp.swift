fd47310b521cf580b5bf1975ecf2934be41e5589
Alexander Ryzhenkov <alex2505@bk.ru>
2025-07-01 20:36:18 +0300

    Initial Commit

---
+++ backAppSui/backAppSuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  backAppSuiApp.swift
+//  backAppSui
+//
+//  Created by Daz N311 on 01.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct backAppSuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
