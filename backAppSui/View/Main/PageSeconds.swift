0df2442c9aa62c4d5ca9d4067504f47c13fbb116
Alexander Ryzhenkov <alex2505@bk.ru>
2025-07-02 11:18:24 +0300

    upd all

---
+++ backAppSui/View/Main/PageSeconds.swift
@@ -0,0 +1,40 @@
+//
+//  PageSeconds.swift
+//  backAppSui
+//
+//  Created by Daz N311 on 02.07.2025.
+//
+
+import SwiftUI
+
+struct PageSeconds: View {
+    var body: some View {
+      VStack(alignment: .leading) {
+        HStack{
+          Text("Профессиональный хоумстейджинг".capitalized)
+            .font(type: .bold,size: 14)
+            .foregroundColor(Color(.gray))
+        }
+        .padding(.horizontal,20)
+        
+        HStack{
+          Text("увеличивает стоимость объекта от 20%")
+            .font(type: .bold,size: 14)
+            .foregroundColor(Color(.gray))
+        }
+        .padding(.horizontal,20)
+        HStack{
+          Text("и ускоряет сделку в 3 раза \(UIScreen.main.bounds.width/2 - 100)")
+            .font(type: .bold,size: 14)
+            .foregroundColor(Color(.gray))
+        }
+        .padding(.horizontal,20)
+        
+      }
+      .position(x: UIScreen.main.bounds.width / 2)
+    }
+}
+
+#Preview {
+    PageSeconds()
+}
