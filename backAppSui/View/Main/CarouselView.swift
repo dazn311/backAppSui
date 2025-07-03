c9ff6ca63444f37ba6742b80c1fa28129f187f08
Alexander Ryzhenkov <alex2505@bk.ru>
2025-07-03 11:11:28 +0300

    upd styles pages[250703-1111]

---
+++ backAppSui/View/Main/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  backAppSui
+//
+//  Created by Daz N311 on 02.07.2025.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
---
+++ backAppSui/View/Main/PageThree.swift
@@ -0,0 +1,18 @@
+//
+//  PageThree.swift
+//  backAppSui
+//
+//  Created by Daz N311 on 02.07.2025.
+//
+
+import SwiftUI
+
+struct PageThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageThree()
+}
